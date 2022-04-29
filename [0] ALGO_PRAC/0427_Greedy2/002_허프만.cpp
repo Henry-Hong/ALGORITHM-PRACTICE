64575850d8bd84502e40de4866cfb89111acfb4a
Henry-Hong <hhr8540@naver.com>
2022-04-29 20:40:15 +0900

    Update 허프만코드: 함수형으로

--- [0] ALGO_PRAC/0427_Greedy2/002_허프만.cpp
+++ [0] ALGO_PRAC/0427_Greedy2/002_허프만.cpp
@@ -6,11 +6,6 @@
 #include <queue>
 using namespace std;
 
-int n;
-string str;
-string codeword[26]; // for lowercase
-string CODEWORD[26]; // for uppercase
-
 typedef struct node* nodePointer;
 typedef struct node{
   char key;
@@ -20,6 +15,12 @@ typedef struct node{
 
 struct compare { bool operator()(nodePointer p, nodePointer q) { return p->frequency > q->frequency; } };
 typedef priority_queue<nodePointer, vector<nodePointer>, compare> PriorityQueue;
+
+int n;
+string str;
+string codeword[26]; // for lowercase
+string CODEWORD[26]; // for uppercase
+nodePointer ROOT = NULL;
 
 void preorder(nodePointer root){
   if(root == NULL) return;
@@ -35,38 +36,63 @@ void inorder(nodePointer root){
   inorder(root->right);
 }
 
-void encode(nodePointer root, string code){
+nodePointer makeNewNode(char key, int frequency, nodePointer l, nodePointer r){
+  nodePointer newNode = new node();
+  newNode->key = key;
+  newNode->frequency = frequency;
+  newNode->left = l;
+  newNode->right = r;
+  return newNode;
+}
+
+nodePointer makeHuffmanTree(vector<char> &S, vector<int> &F, PriorityQueue &pq){
+  // make nodes ready
+  for(int i=1; i<=n; i++)
+    pq.push(makeNewNode(S[i], F[i], NULL, NULL));
+
+  // make Huffman tree
+  for(int i=1; i<n; i++){
+    nodePointer a = pq.top(); pq.pop();
+    nodePointer b = pq.top(); pq.pop();
+    pq.push(makeNewNode('+', a->frequency + b->frequency, a, b));
+  }
+
+  return pq.top();
+}
+
+void makeCodeword(nodePointer root, string code){
   if(root == NULL) return;
+
   if(root->left == NULL && root->right == NULL) {
     if(root->key >= 'a') codeword[root->key - 'a'] = code;
     else CODEWORD[root->key - 'A'] = code;
   }
   else {
-    encode(root->left, code + "0");
-    encode(root->right, code + "1");
+    makeCodeword(root->left, code + "0");
+    makeCodeword(root->right, code + "1");
   }
 }
 
-void decode(nodePointer root, nodePointer ROOT, string code, int idx){
+void encode(string str){
+  for(int i=0; i<str.size(); i++){
+    if(str[i] >= 'a') cout << codeword[str[i] - 'a'];
+    else cout << CODEWORD[str[i] - 'A'];
+  }
+}
+
+void decode(nodePointer root, string code, int idx){
   if(root == NULL) return;
   if(root->left == NULL && root->right == NULL){
     cout << root->key;
-    if(idx >= code.length()) return;
-    decode(ROOT, ROOT, code, idx);
-    return;
+    if(idx >= code.length()) return; // unavoidable
+    decode(ROOT, code, idx); return;
   }
-  switch(code[idx]){
-    case '0':
-      decode(root->left, ROOT, code, idx + 1);
-      break;
-    case '1': 
-      decode(root->right, ROOT, code, idx + 1);
-      break;
-  }
+  decode(code[idx] == '0' ? root->left : root->right, code, idx + 1);
 }
 
 int main(){
 
+  // inputs
   cin >> n;
   vector<char> S(n+1);    // String
   vector<int> F(n+1);     // Frequency
@@ -75,52 +101,28 @@ int main(){
   for(int i=1; i<=n; i++) cin >> S[i];
   for(int i=1; i<=n; i++) cin >> F[i];
   
-  // initialize
-  for(int i=1; i<=n; i++){
-    nodePointer t = new node();
-    t->key = S[i];
-    t->frequency = F[i];
-    t->left = t->right = NULL;
-    pq.push(t);
-  }
-  
-  // make Huffman tree
-  for(int i=1; i<n; i++){
-    nodePointer a = pq.top(); pq.pop();
-    nodePointer b = pq.top(); pq.pop();
-    nodePointer ab = new node();
-    ab->key = '+';
-    ab->frequency = a->frequency + b->frequency;
-    ab->left = a;
-    ab->right = b;
-    pq.push(ab);
-  }
-
-  nodePointer root = pq.top(); pq.pop();
+  // make huffman tree
+  ROOT = makeHuffmanTree(S, F, pq);
 
   // traverse
-  preorder(root); cout << "\n";
-  inorder(root); cout << "\n";
+  preorder(ROOT); cout << "\n";
+  inorder(ROOT); cout << "\n";
 
-  // make Huffman Code
-  encode(root, "");
+  // make Huffman Code (makeCodeWord)
+  makeCodeword(ROOT, "");
 
   // encode
   cin >> n;
   for(int i=0; i<n; i++){
     cin >> str;
-    for(int j=0; j<str.size(); j++){
-      if(str[j] >= 'a') cout << codeword[str[j] - 'a'];
-      else cout << CODEWORD[str[j] - 'A'];
-    }
-    cout << "\n";
+    encode(str); cout << "\n";
   }
 
   // decode
   cin >> n;
   for(int i=0; i<n; i++){
     cin >> str;
-    decode(root, root, str, 0); cout << (i != n-1 ? "\n" : "");
+    decode(ROOT, str, 0); cout << "\n";
   }
 
   return 0;
